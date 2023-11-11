3b22f2572a7c401df9b19f0422bfb9a3d621a2f1
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-11 12:19:31 +0200

    Add some comments

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
+// Array to store user clicked pattern
 const userClickedPattern = [];
 
+// Possible button colors
 const buttonColors = ["red", "blue", "green", "yellow"];
 
+// Current level of the game
 let level = 0;
 
+// Boolean to track if the game has started
 let started = false;
+
+// Counter for user clicks in the current level
 let userClickCounter = 0;
